aa439ea16d16d0132e8c88a4b290f22d0d7770f9
Héctor <hartisca91@gmail.com>
2024-06-03 16:34:20 +0200

    Block user and filter chat added

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,12 +1,39 @@
-import { auth } from '../../lib/firebase'
+import { auth, db } from '../../lib/firebase'
+import { useChatStore } from '../../lib/chatStore'
+import { useUserStore } from '../../lib/userStore'
 import './detail.css'
+import { arrayRemove, arrayUnion, doc, updateDoc } from 'firebase/firestore'
 
 const Detail = () => {
+
+  const { chatId, user, isCurrentUserBlocked, isReceiverBlocked, changeBlock, resetChat } = useChatStore() 
+  const { currentUser } = useUserStore()
+
+  const handleBlock = async () => {
+    if (!user) return;
+
+    const userDocRef = doc(db, "users", currentUser.id);
+
+    try {
+      await updateDoc(userDocRef, {
+        blocked: isReceiverBlocked ? arrayRemove(user.id) : arrayUnion(user.id),
+      });
+      changeBlock();
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleLogout = () => {
+    auth.signOut();
+    resetChat()
+  };
+  
   return (
     <div className='detail'>
       <div className="user">
-        <img src="./avatar.png" alt="" />
-        <h2>Jane Doe</h2>
+        <img src={ user?.avatar || "./avatar.png" } alt="" />
+        <h2>{user?.username}</h2>
         <p>Lorem ipsum dolor sit amet</p>
       </div>
       <div className="info">
@@ -43,8 +70,14 @@ const Detail = () => {
             <img src="./arrowUp.png" alt="" />
           </div>          
         </div>
-        <button>Block User</button>
-        <button className='logOut' onClick={ () => auth.signOut() }>Log Out</button>
+        <button onClick={handleBlock}>
+          {isCurrentUserBlocked
+            ? "You are Blocked!"
+            : isReceiverBlocked
+            ? "User blocked"
+            : "Block User"}
+        </button>
+        <button className='logOut' onClick={ handleLogout }>Log Out</button>
       </div>
     </div>
   )
